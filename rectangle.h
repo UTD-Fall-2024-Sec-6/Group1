a40d7aeeda2f7fab6f388d5413a56be184a538f0
ChaseBuerger <149910362+ChaseBuerger@users.noreply.github.com>
2024-09-06 14:24:24 -0500

    Add files via upload

---
+++ rectangle.h
@@ -0,0 +1,28 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+class Rectangle {
+private:
+    double length;
+    double width;
+
+public:
+    // Constructors
+    Rectangle();
+    Rectangle(double l, double w);
+
+    // Getters
+    double getLength() const;
+    double getWidth() const;
+
+    // Setters
+    void setLength(double l);
+    void setWidth(double w);
+
+    // Methods
+    double getArea() const;
+    double getPerimeter() const;
+};
+
+#endif // RECTANGLE_H
+rectangle.h
