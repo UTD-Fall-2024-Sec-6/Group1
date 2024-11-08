5980a5e292bc30211c75b43ab8efc730013684d4
ChaseBuerger <149910362+ChaseBuerger@users.noreply.github.com>
2024-11-08 14:22:01 -0600

    Update Budget_Tester.java

--- Phase_4_TestCases/Budget_Tester.java
+++ Phase_4_TestCases/Budget_Tester.java
@@ -9,69 +9,55 @@ class Budget_Tester
     void UC7_TC1() 
     {
         Budget budget = new Budget(1000.00);
-        assertEquals(budget.createInsight(14, "weekly", true), "Trends displayed successfully");
-    }
-
-    @Test 
-    void UC7_TC2() 
-    {
-        Budget budget = new Budget(1000.00);
-        assertEquals(budget.createInsight(14, "weekly", false), "Transaction canceled");
+        assertEquals(budget.createInsight(14, "weekly"), "Trends displayed successfully");
     }
 
     @Test 
     void UC7_TC5() 
     {
         Budget budget = new Budget(1000.00);
-        assertEquals(budget.createInsight(7, "weekly", true), "Error: Insufficient Data");
+        assertEquals(budget.createInsight(3, "weekly"), "Error: Insufficient Data");
     }
 
     @Test 
     void UC7_TC3() 
     {
         Budget budget = new Budget(1000.00);
-        assertEquals(budget.createInsight(7, "weekly", true), "Error: Invalid frequency");
+        assertEquals(budget.createInsight(3, "hourly"), "Error: Invalid frequency");
     }
 
     @Test 
     void UC7_TC9() 
     {
         Budget budget = new Budget(1000.00);
-        assertEquals(budget.createInsight(30, "weekly", true), "Error: Data processing issue");
+        assertEquals(budget.createInsight(30, "weekly"), "Error: Data processing issue");
     }
 
     @Test 
     void UC9_TC1() 
     {
         Budget budget = new Budget(1000.00);
-        assertEquals(budget.setInsight(500.00, "short-term", true), "Financial goal set successfully");
-    }
-
-    @Test 
-    void UC9_TC2() 
-    {
-        Budget budget = new Budget(1000.00);
-        assertEquals(budget.setInsight(500.00, "short-term", false), "Transaction canceled");
+        assertEquals(budget.createGoal(500.00, "short-term"), "Financial goal set successfully");
     }
 
     @Test 
     void UC9_TC5() 
     {
         Budget budget = new Budget(1000.00);
-        assertEquals(budget.setInsight(-100.00, "short-term", true), "Error: Invalid financial goal amount");
+        assertEquals(budget.createGoal(-100.00, "short-term"), "Error: Invalid financial goal amount");
     }
 
     @Test 
     void UC9_TC9() 
     {
         Budget budget = new Budget(1000.00);
-        assertEquals(budget.setInsight(100000000.00, "long-term", true), "Error: Financial goal exceeds limit");
+        assertEquals(budget.createGoal(100000000.00, "long-term"), "Error: Financial goal exceeds limit");
     }
 
     @Test 
     void UC9_TC3() 
     {
         Budget budget = new Budget(1000.00);
-        assertEquals(budget.setInsight(500.00, "weekly", true), "Error: Invalid goal timeframe");
+        assertEquals(budget.createGoal(500.00, "weekly"), "Error: Invalid goal timeframe");
     }
 }
