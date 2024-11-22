a1f1f793e81a56f6af7db35da89d11e077dd07ff
Amogh Anoo <141118934+anooamogh@users.noreply.github.com>
2024-11-22 11:21:11 -0600

    Update BudgetController.java

--- BudgetController.java
+++ BudgetController.java
@@ -1,13 +1,44 @@
 public class BudgetController {
-    public void editBudget(Budget budget) {
+    private DBMgr dbMgr;
 
+    public BudgetController(DBMgr dbMgr) {
+        this.dbMgr = dbMgr;
     }
 
-    public void exportBudget(Budget budget) {
+    public boolean editBudget(String username, Budget newBudget) {
+        User user = dbMgr.getUserByUsername(username);
 
+        if (user != null) {
+            user.setBudget(newBudget);
+            return dbMgr.saveBudget(dbMgr.getUIDByUsername(username), user.getBudget());
+        }
+
+        return false;
     }
 
-    public boolean submitGoal(float goal) {
-        return true;
+    public void requestBudgetSummary(String username) {
+        System.out.println("Fetching budget for user: " + username);
+
+        // Retrieve the user's UID from the DB
+        String uid = dbMgr.getUIDByUsername(username);
+        if (uid == null) {
+            System.out.println("Error: No user found with the provided username.");
+            return;
+        }
+
+        // Retrieve budget data
+        Budget budgetData = dbMgr.getBudget(uid);
+        if (budgetData == null) {
+            System.out.println("Error: No Budget Data Found.");
+            return;
+        }
+
+        // Process budget summary
+        String summaryData = budgetData.processBudgetSummary();
+        if (summaryData != null) {
+            System.out.println("Budget Summary Displayed:\n" + summaryData);
+        } else {
+            System.out.println("Error: Failed to process budget summary.");
+        }
     }
 }
